3e0eb29e9cf25b7e060f0afa056d9573cd269d82
zjun122 <3282073436@qq.com>
2023-03-31 08:37:58 +0800

    Initial commit

---
+++ src/test/java/com/zjun122/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjun122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
